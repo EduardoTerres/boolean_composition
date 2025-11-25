58d43be02a1a410a0ba13dd08808e4f21537b7b2
EduardoTerres <EduardoTerres@users.noreply.github.com>
2025-11-25 23:13:22 +0100

    cleanup

--- four_rooms/extension/plots.py
+++ four_rooms/extension/plots.py
@@ -21,66 +21,9 @@ from four_rooms.config import Config_4, Config_8, Config_16
 
 random.seed(42)
 
-def plot(
-    num_rooms,
-    tasks,
-    save_name = None,
-):
-    tasks = ["\n".join(str(goal) for goal in task) for task in tasks]
-    plt.ylim(-0.5, 2)
-    rc_ = {
-        "figure.figsize": (30, 10),
-        "axes.labelsize": 30,
-        "font.size": 30,
-        "legend.fontsize": 20,
-        "axes.titlesize": 30,
-    }
-    sns.set(rc=rc_, style="darkgrid", font_scale=1.8)
-    rc("text", usetex=False)
-
-    n = 2
-
-    data = dd.io.load(f"exps_data_extension/exp2_all_returns_{num_rooms}.h5")
-    data = [data[t] / 10 for t in range(len(data))]
-
-    types = [
-        "Sparse rewards and Same absorbing set",
-        "Dense rewards and Same absorbing set",
-        "Sparse rewards and Different absorbing set",
-        "Dense rewards and Different absorbing set",
-    ]
-
-    data = pd.DataFrame(
-        [[data[0][i, t] for t in range(n, len(tasks))] + [types[0]] for i in range(len(data[0]))]
-        + [[data[1][i, t] for t in range(n, len(tasks))] + [types[1]] for i in range(len(data[1]))]
-        + [[data[2][i, t] for t in range(n, len(tasks))] + [types[2]] for i in range(len(data[2]))]
-        + [[data[3][i, t] for t in range(n, len(tasks))] + [types[3]] for i in range(len(data[3]))],
-        columns=tasks[n:] + ["Domain"],
-    )
-    data = pd.melt(data, "Domain", var_name="Tasks", value_name="Average Returns")
-
-    fig, ax = plt.subplots()
-    ax = sns.boxplot(
-        x="Tasks",
-        y="Average Returns",
-        hue="Domain",
-        data=data,
-        linewidth=3,
-        showfliers=False,
-    )
-    if save_name is None:
-        save_name = f"four_rooms/extension/figures/exp2_output_{num_rooms}.png"
-    fig.savefig(save_name, bbox_inches="tight")
-    print(f"Figure saved to {save_name}")
-
-# if __name__ == "__main__":
-#     from four_rooms.config import Config_4
-#     print(Config_4["Tasks"])
-#     plot(num_rooms=4, tasks=Config_4["Tasks"])
-
 def generate_plot_time_taken_all_num_goals():
-    num_runs = 1000
-    maxiter = 50
+    num_runs = 10_000
+    maxiter = 5_000
 
     time_taken_4 = dd.io.load(f"exps_data_extension/composed_time_taken_4_{maxiter}_{num_runs}.h5")
     time_taken_8 = dd.io.load(f"exps_data_extension/composed_time_taken_8_{maxiter}_{num_runs}.h5")
@@ -104,7 +47,7 @@ def generate_plot_num_value_functions_learned():
 
 def generate_plot_extended_q_value(on_off: str):
     num_rooms = 16
-    maxiter = 1000
+    maxiter = 5000
 
     configs = {
         4: Config_4,
@@ -129,7 +72,7 @@ def generate_plot_returns_optimality():
     optimal_return = {}
     for num_rooms in [4, 8, 16]:
         returns_per_maxiter[num_rooms] = dd.io.load(f"exps_data_extension/convergence_returns_{num_rooms}.h5")
-        optimal_returns = dd.io.load(f"exps_data_extension/composed_returns_{num_rooms}_1000_10000.h5")
+        optimal_returns = dd.io.load(f"exps_data_extension/composed_returns_{num_rooms}_5000_10000.h5")
         optimal_return[num_rooms] = mean([mean(optimal_returns[task]["optimal"]) for task in optimal_returns])
 
     plot_returns_optimality_all_num_goals(
@@ -139,9 +82,9 @@ def generate_plot_returns_optimality():
     )
 
 if __name__ == "__main__":
-    # generate_plot_time_taken_all_num_goals()
+    generate_plot_time_taken_all_num_goals()
     generate_plot_returns_all_num_goals()
-    # generate_plot_returns_optimality()
-    # generate_plot_num_value_functions_learned()
-    # generate_plot_extended_q_value(on_off="off")
-    # generate_plot_extended_q_value(on_off="on")
+    generate_plot_returns_optimality()
+    generate_plot_num_value_functions_learned()
+    generate_plot_extended_q_value(on_off="off")
+    generate_plot_extended_q_value(on_off="on")
