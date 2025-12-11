5fae8a2eb2539df0182b5f1f5c09d3a9916081d5
EduardoTerres <169451532+EduardoTerres@users.noreply.github.com>
2025-12-11 12:56:11 +0100

    Readme and order plots

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
-Code for the paper "A Boolean Task Algebra For Reinforcement Learning"
+Code for a extension providing further insights in the paper "A Boolean Task Algebra For Reinforcement Learning"
 
-https://arxiv.org/abs/2001.01394
+## Training tasks
+Run
+`
+uv run four_rooms/extension/exp_train.py
+`
+specifying the number of rooms you want to run the experiment for
+(e.g. `NUM_ROOMS = 8`).
 
-http://www.betr-rl.ml/2020/abs/25/
+## Training tasks by increasing level of optimality
+Run
+`
+uv run four_rooms/extension/exp_convergence.py
+`
+
+## Plots (all)
+
+Run
+`
+uv run four_rooms/extension/plots.py
+`
--- four_rooms/extension/plots.py
+++ four_rooms/extension/plots.py
@@ -29,25 +29,25 @@ def generate_plot_time_taken_all_num_goals():
     time_taken_8 = dd.io.load(f"exps_data_extension/composed_time_taken_8_{maxiter}_{num_runs}.h5")
     time_taken_16 = dd.io.load(f"exps_data_extension/composed_time_taken_16_{maxiter}_{num_runs}.h5")
     time_taken = {4: time_taken_4, 8: time_taken_8, 16: time_taken_16}
-    plot_time_taken_all_num_goals(time_taken, save_name=f"four_rooms/extension/figures/time_taken_all_num_goals_{maxiter}_{num_runs}.png")
+    plot_time_taken_all_num_goals(time_taken, save_name=f"four_rooms/extension/figures/time_taken_all_num_goals_{maxiter}_{num_runs}.pdf")
 
 def generate_plot_returns_all_num_goals():
     maxiter = 5_000
     num_runs = 10_000
 
     returns_4 = dd.io.load(f"exps_data_extension/composed_returns_4_{maxiter}_{num_runs}.h5")
-    returns_8 = dd.io.load(f"exps_data_extension/composed_returns_8_{maxiter}_{num_runs}.h5")
+    returns_8 = dd.io.load(f"exps_data_extension/composed_returns_8_{maxiter}_{num_runs // 10}.h5")
     returns_16 = dd.io.load(f"exps_data_extension/composed_returns_16_{maxiter}_{num_runs}.h5")
     returns = {4: returns_4, 8: returns_8, 16: returns_16}
-    plot_returns_all_num_goals(returns, save_name=f"four_rooms/extension/figures/returns_all_num_goals_{maxiter}_{num_runs}.png")
+    plot_returns_all_num_goals(returns, save_name=f"four_rooms/extension/figures/returns_all_num_goals_{maxiter}_{num_runs}.pdf")
 
 def generate_plot_num_value_functions_learned():
-    save_name = "four_rooms/extension/figures/num_value_functions_learned.png"
+    save_name = "four_rooms/extension/figures/num_value_functions_learned.pdf"
     plot_num_value_functions_learned(save_name)
 
 def generate_plot_extended_q_value(on_off: str):
     num_rooms = 16
-    maxiter = 5000
+    maxiter = 1000
 
     configs = {
         4: Config_4,
@@ -67,7 +67,6 @@ def generate_plot_extended_q_value(on_off: str):
     plot_extended_q_value(learned_EQ, goals, terminal_states, num_rooms, save_name)
 
 def generate_plot_returns_optimality():
-
     returns_per_maxiter = {}
     optimal_return = {}
     for num_rooms in [4, 8, 16]:
@@ -78,13 +77,13 @@ def generate_plot_returns_optimality():
     plot_returns_optimality_all_num_goals(
         returns_per_maxiter,
         optimal_return,
-        save_name="four_rooms/extension/figures/returns_optimality.png",
+        save_name="four_rooms/extension/figures/returns_optimality.pdf",
     )
 
 if __name__ == "__main__":
-    generate_plot_time_taken_all_num_goals()
-    generate_plot_returns_all_num_goals()
-    generate_plot_returns_optimality()
-    generate_plot_num_value_functions_learned()
-    generate_plot_extended_q_value(on_off="off")
-    generate_plot_extended_q_value(on_off="on")
+    generate_plot_extended_q_value(on_off="off")  # Figure 2
+    generate_plot_extended_q_value(on_off="on")  # Figure 2
+    generate_plot_returns_all_num_goals()  # Figure 3
+    generate_plot_returns_optimality()  # Figure 4
+    generate_plot_time_taken_all_num_goals()  # Figure 5
+    generate_plot_num_value_functions_learned()  # Figure 6
